609decfc740dd6080842e85c07bce29dc24959de
Lc3586 <2445624023@qq.com>
2020-12-11 14:18:11 +0800

    新增

---
+++ src/Library/FreeSql/Extention/CacheExtention.cs
@@ -0,0 +1,27 @@
+﻿using FreeSql.Internal.Model;
+using Microsoft.OpenApi.Any;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.FreeSql.Extention
+{
+    /// <summary>
+    /// 缓存
+    /// </summary>
+    internal static class CacheExtention
+    {
+        /// <summary>
+        /// 模型对应的表字段信息
+        /// </summary>
+        public static Dictionary<Type, TableInfo> TableInfoDic = new Dictionary<Type, TableInfo>();
+
+        ///// <summary>
+        ///// 销毁
+        ///// </summary>
+        //public static void Dispose()
+        //{
+        //    TableInfoDic = new Dictionary<Type, TableInfo>();
+        //}
+    }
+}
