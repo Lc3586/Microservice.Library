56c6b899e8fc21fdf90b1c225d570b4531054c5c
WDNMD <34331208+Lc3586@users.noreply.github.com>
2021-03-10 16:20:44 +0800

    ~

--- src/Library/WeChat/Application/WeChatServiceCollectionExtensions.cs
+++ src/Library/WeChat/Application/WeChatServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using Microservice.Library.WeChat.Application;
 using Microservice.Library.WeChat.Gen;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using Senparc.CO2NET.Cache;
+using Senparc.CO2NET.RegisterServices;
+using Senparc.Weixin.RegisterServices;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,10 +23,12 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <para>单例模式</para>
         /// </summary>
         /// <param name="services"></param>
+        /// <param name="configuration"></param>
         /// <param name="setupAction"></param>
         /// <returns></returns>
         public static IServiceCollection AddWeChatService(
             this IServiceCollection services,
+            IConfiguration configuration,
             Action<WeChatGenOptions> setupAction = null)
         {
             //注册自定义配置程序，将高级配置（<WeChatGenOptions）应用于低级配置（WeChatBaseOptions, WeChatDevOptions, WeChatOAuthOptions）。
@@ -36,6 +41,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddSingleton<IWeChatServiceProvider, WeChatServiceGenerator>();
 
+            services.AddSenparcGlobalServices(configuration)
+                .AddSenparcWeixinServices(configuration);
             services.AddMemoryCache();
 
             if (setupAction != null) services.ConfigureWeChatService(setupAction);
