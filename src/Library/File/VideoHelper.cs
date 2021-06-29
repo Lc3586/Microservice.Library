5579a41e5e8cf02ac5b256059fa90707ed1c6a46
WDNMD <34331208+Lc3586@users.noreply.github.com>
2021-06-29 13:58:30 +0800

    ~

--- src/Library/File/VideoHelper.cs
+++ src/Library/File/VideoHelper.cs
@@ -1,12 +1,10 @@
-﻿using ImageProcessorCore;
+﻿using Microservice.Library.Extension;
 using Microservice.Library.File.Model;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microservice.Library.File
@@ -34,9 +32,36 @@ namespace Microservice.Library.File
                 throw new ApplicationException($"当前操作系统不支持使用{name}.");
 
             if (!System.IO.File.Exists(path))
-                throw new ApplicationException($"所需的文件不存在: {path}");
+                throw new ApplicationException($"所需的文件不存在: {path}.");
 
             return path;
+        }
+
+        /// <summary>
+        /// 获取进程
+        /// </summary>
+        /// <param name="filename">文件绝对路径</param>
+        /// <param name="arguments">参数</param>
+        /// <returns></returns>
+        private static Process GetProcess(string filename, string arguments)
+        {
+            if (!filename.Exists())
+                throw new ApplicationException($"指定文件不存在: {filename}.");
+
+            var process = new Process();
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            process.StartInfo.RedirectStandardOutput = true;
+            //process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+            process.StartInfo.RedirectStandardError = true;
+            //process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+
+            process.StartInfo.FileName = filename;
+            process.StartInfo.Arguments = arguments;
+
+            return process;
         }
 
         /// <summary>
@@ -46,56 +71,45 @@ namespace Microservice.Library.File
         /// <para>在windows系统下运行时，需要将ffmpeg.exe文件放置于应用根目录下。</para>
         /// <para>在linux系统下运行时，需要安装ffmpeg，读取目录为(/usr/bin/ffmpeg)。</para>
         /// </remarks>
-        /// <param name="videofile">视频文件绝对路径</param>
-        /// <param name="imagefile">截图文件存储路径</param>
+        /// <param name="videoFile">视频文件绝对路径</param>
+        /// <param name="ffmpegFile">ffmpeg应用程序文件绝对路径</param>
+        /// <param name="imageFile">截图文件存储路径</param>
         /// <param name="time">指定时间</param>
         /// <param name="quality">图片质量[2-31]</param>
         /// <param name="width">图片宽度</param>
         /// <param name="height">图片高度</param>
-        public static async Task Screenshot(this string videofile, string imagefile, TimeSpan time, int quality = 31, int? width = null, int? height = null)
+        public static async Task Screenshot(this string videoFile, string ffmpegFile, string imageFile, TimeSpan time, int quality = 31, int? width = null, int? height = null)
         {
-            if (!videofile.Exists())
-                throw new ApplicationException($"视频文件不存在[{videofile}].");
+            if (!videoFile.Exists())
+                throw new ApplicationException($"视频文件不存在[{videoFile}].");
 
-            string path = GetEXE();
+            //string path = GetEXE();
 
-            var ifDir = new FileInfo(imagefile).DirectoryName;
+            var ifDir = new FileInfo(imageFile).DirectoryName;
             if (!Directory.Exists(ifDir))
                 Directory.CreateDirectory(ifDir);
 
-            var arguments = $" -ss {time} -i \"{videofile}\" -q:v {quality} -frames:v 1 -an -y -f mjpeg";
+            var arguments = $" -ss {time} -i \"{videoFile}\" -q:v {quality} -frames:v 1 -an -y -f mjpeg";
 
             if (width.HasValue && height.HasValue)
-                arguments += $" -s {width.Value}x{height.Value} \"{imagefile}\"";
+                arguments += $" -s {width.Value}x{height.Value} \"{imageFile}\"";
             else
-                arguments += $" -s \"{imagefile}\"";
+                arguments += $" -s \"{imageFile}\"";
 
-            using (var process = new Process())
+            using (var process = GetProcess(ffmpegFile, arguments))
             {
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-#if DEBUG
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-                process.StartInfo.RedirectStandardError = true;
-                process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
-#else
-                process.StartInfo.RedirectStandardOutput = false;
-                process.StartInfo.RedirectStandardError = false;
-#endif
-                process.StartInfo.FileName = path;
-                process.StartInfo.Arguments = arguments;
                 process.Start();
-#if DEBUG
+
                 var output = await process.StandardOutput.ReadToEndAsync();
                 var error = await process.StandardError.ReadToEndAsync();
-#endif
+
                 process.WaitForExit();
 #if DEBUG
                 Console.Write(output);
                 Console.Write(error);
 #endif
+                if (!error.IsNullOrWhiteSpace())
+                    throw new ApplicationException($"截图失败.{error}");
             }
         }
 
@@ -106,21 +120,22 @@ namespace Microservice.Library.File
         /// <para>在windows系统下运行时，需要将ffprobe.exe文件放置于应用根目录下。</para>
         /// <para>在linux系统下运行时，需要安装ffmpeg，读取目录为(/usr/bin/ffprobe)。</para>
         /// </remarks>
-        /// <param name="videofile">视频文件绝对路径</param>
+        /// <param name="videoFile">视频文件绝对路径</param>
+        /// <param name="ffprobeFile">fffprobe应用程序文件绝对路径</param>
         /// <param name="format">获取有关输入多媒体流的容器格式的信息</param>
         /// <param name="streams">获取有关输入多媒体流中包含的每个媒体流的信息</param>
         /// <param name="chapters">获取有关以该格式存储的章节的信息</param>
         /// <param name="programs">获取有关程序及其输入多媒体流中包含的流的信息</param>
         /// <param name="version">获取与程序版本有关的信息、获取与库版本有关的信息、获取与程序和库版本有关的信息</param>
         /// <returns></returns>
-        public static async Task<VideoInfo> GetVideoInfo(this string videofile, bool format = true, bool streams = true, bool chapters = false, bool programs = false, bool version = false)
+        public static async Task<VideoInfo> GetVideoInfo(this string videoFile, string ffprobeFile, bool format = true, bool streams = true, bool chapters = false, bool programs = false, bool version = false)
         {
-            if (!videofile.Exists())
-                throw new ApplicationException($"视频文件不存在[{videofile}].");
+            if (!videoFile.Exists())
+                throw new ApplicationException($"视频文件不存在[{videoFile}].");
 
-            string path = GetEXE("ffprobe");
+            //string path = GetEXE("ffprobe");
 
-            var arguments = $" -i {videofile} -print_format json -show_data";
+            var arguments = $" -i {videoFile} -print_format json -show_data";
 
             if (format)
                 arguments += " -show_format";
@@ -133,32 +148,21 @@ namespace Microservice.Library.File
             if (version)
                 arguments += " -show_versions";
 
-            using (var process = new Process())
+            using (var process = GetProcess(ffprobeFile, arguments))
             {
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-#if DEBUG
-                process.StartInfo.RedirectStandardError = true;
-#else
-                process.StartInfo.RedirectStandardError = false;
-                process.StartInfo.StandardErrorEncoding = Encoding.UTF8;
-#endif
-                process.StartInfo.FileName = path;
-                process.StartInfo.Arguments = arguments;
                 process.Start();
 
                 var output = await process.StandardOutput.ReadToEndAsync();
-#if DEBUG
                 var error = await process.StandardError.ReadToEndAsync();
-#endif
+
                 process.WaitForExit();
 #if DEBUG
                 Console.Write(output);
                 Console.Write(error);
 #endif
+                if (!error.IsNullOrWhiteSpace())
+                    throw new ApplicationException($"获取视频信息失败.{error}");
+
                 return JsonConvert.DeserializeObject<VideoInfo>(output);
             }
         }
